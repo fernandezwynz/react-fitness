d26ae69f4f70d3baad450aa70aed09197d58f3bb
fernandezwynz <bddwynz@gmail.com>
2025-02-19 00:02:44 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'; // Import Navbar
+import Nav from 'react-bootstrap/Nav'; // Import Nav
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { UserProvider } from './context/UserContext';
@@ -45,6 +47,17 @@ function App() {
     <UserProvider value={{ user, setUser, unsetUser }}>
       <WorkoutProvider>
         <Router>
+          <Navbar bg="light" expand="lg" className="navbar">
+            <Container>
+              <Navbar.Brand href="/">Fitness App</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/users/login">Login</Nav.Link>
+                <Nav.Link href="/users/register">Register</Nav.Link>
+                <Nav.Link href="/workouts">Workouts</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
           <Container>
             <Routes>
               <Route path="/" element={<HomePage />} />
